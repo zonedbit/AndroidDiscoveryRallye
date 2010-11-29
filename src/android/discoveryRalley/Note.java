7f702474f3880633052d77fa232cea743d0b2b3a
mrmiyagi <zonedbit@googlemail.com>
2010-11-29 18:35:31 +0100

    Implementing reading and writing
    The note will be saved in the internal storage
    and is ionly retrieved if the onCreate method called.

--- src/android/discoveryRalley/Note.java
+++ src/android/discoveryRalley/Note.java
@@ -1,9 +1,25 @@
 package android.discoveryRalley;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class Note extends Activity{
+	
+	/**
+	 * The filename to store the user notes
+	 */
+	private String FILENAME = "private_notes";
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -12,6 +28,89 @@ public class Note extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.note);
+		
+		// Load the notes of the user
+		restoreNotes();
+	}
+	
+	
+	/**
+	 * 
+	 * Handler to save the user notes
+	 * 
+	 * @param v The Android View
+	 * @throws IOException 
+	 */
+	public void saveNoteHandler(View v) {
+		// TODO Delete me
+		Log.i("DiscoveryRalley","Note::saveNoteHandler(View v)");
+		
+		// TODO Get the text form the EditText field
+		//String string = "hello world!\n";
+		
+		EditText et = (EditText)findViewById(R.id.noteEditTxt);
+		String string = et.getText().toString();
+
+		OutputStreamWriter out = null;
+		try {
+			  // open file for writing
+			  out = 
+				  new OutputStreamWriter(openFileOutput(FILENAME,Context.MODE_PRIVATE));
+			  // write the content to the file
+			  out.write(string);
+			  // close the file
+			} catch (java.io.IOException e) {
+				Log.e("DiscoveryRalley","Note::saveNoteHandler(View v) IOException");
+			} finally {
+				if ( out != null ){
+					try {
+						out.close();
+					} catch (IOException e) {
+						Log.e("DiscoveryRalley",
+								"Note::saveNoteHandler(View v) " +
+								"can not close out stream");
+					}
+				}
+			}
+	}
+	
+	/**
+	 * Read the saved user notes and write it into the EditText field
+	 */
+	private void restoreNotes (){
+		Log.i("DiscoveryRalley","Note::restoreNotes ()");
+		
+		
+		InputStream is = null;
+		try {
+			is = openFileInput(FILENAME);
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			// Reading from the file
+			String line = "";
+			while ( (line=br.readLine()) != null){
+				EditText et = (EditText)findViewById(R.id.noteEditTxt);
+				et.setText(line);
+			}
+
+			
+		} catch (FileNotFoundException e) {
+			Log.e("DiscoveryRalley", 
+					"Note::restoreNotes () FileNotFoundException" );
+		} catch (IOException e) {
+			Log.e("DiscoveryRalley", 
+					"Note::restoreNotes () IOException by reading the file" );
+		} finally {
+			if ( is != null ){
+				try {
+					is.close();
+				} catch (IOException e) {
+					Log.e("DiscoveryRalley", 
+							"Note::restoreNotes () can not close the file input stream" );
+				}
+			}
+		}
 	}
 	
 }
